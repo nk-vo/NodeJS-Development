6be9f31afee00a7afd0d0938c29ac05d6c170c14
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2022-11-03 15:31:57 -0700

    update course

--- http-server/index.js
+++ http-server/index.js
@@ -2,14 +2,37 @@ const http = require("http");
 const PORT = 3000;
 const server = http.createServer()
 
-server.on("request", (req, res) => {
-  if (req.url === "/friend") {
-  // res.writeHead(200, { "Content-Type": "application/json" });
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ id: 1, name: "Isaac Newton" }));
+const friends = [
+  {
+    id: 0,
+    name: 'Nikola Tesla',
+  },
+  {
+    id: 1,
+    name: 'Sir Isaac Newton',
+  },
+  {
+    id: 2,
+    name: 'Albert Einstein',
+  }
+];
 
-  } else if (req.url === '/message'){
+server.on("request", (req, res) => {
+  const items = req.url.split('/')
+  // '/friend/2 => {'', 'friend', '2'}
+  if (items[1] === "/friend") {
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+
+
+  } else if (items[1] === '/message') {
     res.setHeader("Content-Type", "text/html");
     res.write('<html>');
   } else {
