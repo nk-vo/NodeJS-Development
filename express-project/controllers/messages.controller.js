d1da9605bd328a383c614374327d91148c3bf1d8
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2022-11-05 15:49:06 -0700

    update

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,8 +1,12 @@
+const path = require('path')
+
 function getMessages(req, res) {
   res.send("<ul><li>Helloo Albert!</li></ul>");
 }
 
 function postMessage(req, res) {
+  
+  res.sendFile(path.join(__dirname, '..', 'public/images', 'skimountain.jpg'))
   res.send("Updating messages....");
 }
 
