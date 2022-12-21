7c8d5683fd5f8c5d2ee6358a4380bac49295620f
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2022-12-20 18:13:16 -0800

    update nasa project

---
+++ nasa-project/server/src/server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const app = require("./app");
+
+const { loadPlanetData } = require("./models/planets.model");
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+async function startServer() {
+  await loadPlanetData();
+
+  server.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+  });
+}
+
+startServer();
