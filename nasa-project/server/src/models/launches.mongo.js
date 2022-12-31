5f657b7120dd0ed4ef0a4d5c336ae50e783f8752
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2022-12-31 01:05:12 -0800

    connect model to schema

--- nasa-project/server/src/models/launches.mongo.js
+++ nasa-project/server/src/models/launches.mongo.js
@@ -32,3 +32,6 @@ const launchesSchema = new mongoose.Schema({
     default: true,
   },
 });
+
+// Connect launchesSchema to the 'launches' collection in the database
+module.exports = mongoose.model('Launch', launchesSchema);
