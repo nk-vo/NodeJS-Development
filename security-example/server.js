ed36eb23176491e25a71cf83f479ee24777fb603
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2023-01-22 00:51:56 -0800

    setting up google oauth

--- security-example/server.js
+++ security-example/server.js
@@ -3,36 +3,75 @@ const path = require("path");
 const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 
-require('dotenv').config();
+require("dotenv").config();
 const PORT = 3000;
 
 const CONFIG = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: CONFIG.CLIENT_ID,
+  clientSecret: CONFIG.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile", profile);
+  done(null, profile);
+}
+
+passport.use(
+  new Strategy(
+    AUTH_OPTIONS,
+    verifyCallback
+  )
+);
+
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
 
-function checkLoggedIn(req, res, next)  {
+function checkLoggedIn(req, res, next) {
   const isLoggedIn = true;
   if (!isLoggedIn) {
     return res.status(401).json({
       error: "You must log in!",
-    })
+    });
   }
   next();
 }
 
-app.use('/auth/google', (req, res) => {});
+app.use(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email"] })
+);
 
-app.get('/auth/google/callback', (req, res) => {});
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back!");
+  }
+);
 
-app.get('/auth/logout', (req, res) => {});
+app.get("/auth/logout", (req, res) => {});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
+});
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to log in!");
 });
 
 app.get("/", (req, res) => {
@@ -40,10 +79,13 @@ app.get("/", (req, res) => {
 });
 
 https
-  .createServer({
-    key: fs.readFileSync("key.pem"),
-    cert: fs.readFileSync("cert.pem"),
-  }, app)
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
   .listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
