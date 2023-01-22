10c30ae7096d03b1431eadc0422e99cf959df7ce
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2023-01-21 23:48:41 -0800

    authentication endpoints with middleware

--- security-example/server.js
+++ security-example/server.js
@@ -11,7 +11,23 @@ const app = express();
 app.use(helmet());
 app.use(express.static("public"));
 
-app.get("/secret", (req, res) => {
+function checkLoggedIn(req, res, next)  {
+  const isLoggedIn = true;
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in!",
+    })
+  }
+  next();
+}
+
+app.use('/auth/google', (req, res) => {});
+
+app.get('/auth/google/callback', (req, res) => {});
+
+app.get('/auth/logout', (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
 });
 
