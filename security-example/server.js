58aeff0c74939aa455b9e738cb2994dddead3c03
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2023-02-02 00:19:29 -0800

    serialize cookie sessions

--- security-example/server.js
+++ security-example/server.js
@@ -1,27 +1,31 @@
-const fs = require("fs");
-const path = require("path");
-const https = require("https");
-const express = require("express");
-const helmet = require("helmet");
-const passport = require("passport");
-const { Strategy } = require("passport-google-oauth20");
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const express = require('express');
+const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+const {verify} = require('cookie-session');
+const cookieSession = require('cookie-session');
 
-require("dotenv").config();
+require('dotenv').config();
 const PORT = 3000;
 
-const CONFIG = {
+const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
-  callbackURL: "/auth/google/callback",
-  clientID: CONFIG.CLIENT_ID,
-  clientSecret: CONFIG.CLIENT_SECRET,
+  callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
 };
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
-  console.log("Google profile", profile);
+  console.log('Google profile', profile);
   done(null, profile);
 }
 
@@ -32,57 +36,74 @@ passport.use(
   )
 );
 
+// Save the session to cookie
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+// Read the session from the cookie
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
 const app = express();
 
 app.use(helmet());
-app.use(passport.initialize());
 
-function checkLoggedIn(req, res, next) {
+app.use(cookieSession({
+  name: 'session',
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+function checkLoggedIn(req, res, next) { //req.user
   const isLoggedIn = true;
   if (!isLoggedIn) {
     return res.status(401).json({
-      error: "You must log in!",
+      error: 'You must log in!',
     });
   }
   next();
 }
 
 app.use(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["email"] })
+  '/auth/google',
+  passport.authenticate('google', { scope: ['email'] })
 );
 
 app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: "/failure",
-    successRedirect: "/",
-    session: false,
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: true,
   }),
   (req, res) => {
-    console.log("Google called us back!");
+    console.log('Google called us back!');
   }
 );
 
-app.get("/auth/logout", (req, res) => {});
+app.get('/auth/logout', (req, res) => {});
 
-app.get("/secret", checkLoggedIn, (req, res) => {
-  return res.send("Your personal secret value is 42!");
+app.get('/secret', checkLoggedIn, (req, res) => {
+  return res.send('Your personal secret value is 42!');
 });
 
-app.get("/failure", (req, res) => {
-  return res.send("Failed to log in!");
+app.get('/failure', (req, res) => {
+  return res.send('Failed to log in!');
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 https
   .createServer(
     {
-      key: fs.readFileSync("key.pem"),
-      cert: fs.readFileSync("cert.pem"),
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
     },
     app
   )
