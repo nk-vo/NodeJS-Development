9b18e5121cbdd7762b7e056e5d281dbc795d5b2f
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2023-02-10 00:13:50 -0800

    modularize schemas using graphql-tools

--- graphql-example/server.js
+++ graphql-example/server.js
@@ -1,76 +1,35 @@
-const express = require('express');
-const { buildSchema } = require('graphql');
-const { graphqlHTTP } = require('express-graphql');
+const path = require("path");
+const express = require("express");
 
-const schema = buildSchema(`
-  type Query {
-    product: [Product]
-    orders: [Order]
-  }
+const { graphqlHTTP } = require("express-graphql");
 
-  type Product {
-    id: ID!
-    description: String!
-    reviews: [Review]
-    price: Float!
-  }
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 
-  type Review {
-    rating: Int!
-    comment: String
-  }
+const typesArray = loadFilesSync("**/*", {
+  extensions: ["graphql"],
+});
 
-  type Order {
-    date: String!
-    subtotal: Float!
-    items: [OrderItem]
-  }
-
-  type OrderItem {
-    product: Product!
-    quantity: Int!
-  }
-`);
+const schema = makeExecutableSchema({
+  typeDefs: typesArray,
+});
 
 const root = {
-  products: [
-    {
-      id: 'redshoe',
-      description: 'Red Shoes',
-      price: 29.99,
-    },
-    {
-      id: 'bluejean',
-      description: 'Blue Jeans',
-      price: 55.55,
-    },
-  ],
-  orders: [
-    {
-      date: '2005-05-05',
-      subtotal: 90.22,
-      items: [
-        {
-          product: {
-            id: 'redshoe',
-            description: 'Old Red Shoes',
-            price: 45.11,
-          },
-          quantity: 2,
-        },
-      ],
-    },
-  ]
+  products: require("./products/products.model"),
+  orders: require("./orders/orders.model"),
 };
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(3000, () => {
-  console.log('Running GraphQL server...');
+  console.log("Running GraphQL server...");
 });
