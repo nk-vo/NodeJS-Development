5ff240dbf2b691abd3d237847e544d311d0eebba
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2023-02-10 01:21:20 -0800

    replace express-graphql with apollo server

--- graphql-example/server.js
+++ graphql-example/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const { graphqlHTTP } = require('express-graphql');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
@@ -11,18 +11,22 @@ const typesArray = loadFilesSync("**/*", {
 });
 const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
 
-const schema = makeExecutableSchema({
-  typeDefs: typesArray,
-  resolvers: resolversArray,
-});
+async function startApolloServer() {
+  const app = express();
+  
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray,
+  });
 
-const app = express();
+  const server = new ApolloServer({ schema });
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: true,
-}));
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
 
-app.listen(3000, () => {
-  console.log('Running GraphQL server...');
-});
+  app.listen(3000, () => {
+    console.log('Running GraphQL server...');
+  });
+}
+
+startApolloServer();
