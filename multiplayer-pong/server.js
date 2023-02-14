38d9ca9c34fbf6f64f5da458ff1f8abff9bebb7b
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2023-02-14 02:16:55 -0800

    disconnect event

--- multiplayer-pong/server.js
+++ multiplayer-pong/server.js
@@ -21,12 +21,20 @@ io.on('connection', (socket) => {
 
     readyPlayerCount++;
 
-    if (readyPlayerCount === 2) {
+    if (readyPlayerCount % 2) {
       io.emit('startGame', socket.id);
     }
   });
 
   socket.on('paddleMove', (paddleData) => {
     socket.broadcast.emit('paddleMove', paddleData);
+  });
+
+  socket.on('ballMove', (ballData) => {
+    socket.broadcast.emit('ballMove', ballData);
+  });
+
+  socket.on('disconnect', (reason) => {
+    console.log(`Client {socket.id} disconnected: ${reason}`);
   });
 });
