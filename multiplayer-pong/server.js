abe62bbc3c08f4a604bc5abd732c816a30bca3b8
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2023-02-14 02:08:08 -0800

    implement paddle logic

--- multiplayer-pong/server.js
+++ multiplayer-pong/server.js
@@ -11,6 +11,22 @@ const PORT = 3000;
 server.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
+let readyPlayerCount = 0;
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('a user connected', socket.id);
+
+  socket.on('ready', () => {
+    console.log('Player ready', socket.id);
+
+    readyPlayerCount++;
+
+    if (readyPlayerCount === 2) {
+      io.emit('startGame', socket.id);
+    }
+  });
+
+  socket.on('paddleMove', (paddleData) => {
+    socket.broadcast.emit('paddleMove', paddleData);
+  });
 });
